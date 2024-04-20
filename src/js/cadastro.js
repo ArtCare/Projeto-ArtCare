aa5dbff800f3ec157c118c2bbeef9de53266c15b
RyanTorres05 <ryan.costa@sptech.school>
2024-04-19 21:55:48 -0300

    inserindo validações e implementação do FOR

---
+++ src/js/cadastro.js
@@ -0,0 +1,34 @@
+function cadastro() {
+    const nome = input_nome.value
+    const cnpj = input_cnpj.value
+    const email = input_email.value
+    const senha = input_senha.value
+    let senhaNum = false
+
+    if(nome.length <= 2){
+        alert("Seu nome precisa ter mais que 2 caracteres")
+    } else if (cnpj.length != 14){
+        alert("Seu cnpj é precisa ter 14 dígitos")
+    } else if (email.indexOf("@") == -1){
+        alert("Seu email não possui @")
+    } else if (email.indexOf(".") == -1){
+        alert("Seu email precisa ter um ' . ' ")
+    } 
+    
+    if (senha.length <= 4){
+        alert("Sua senha precisa ter mais que 4 caracteres")
+    } else {
+        for(let numero = 0; numero <= 9; numero ++ ){
+            if(senha.indexOf(numero.toString())!= -1){
+                senhaNum = true
+            } 
+        }
+        if(senhaNum == false){
+            alert("Senha precisa de um caracter numérico")
+        } else {
+            alert("Deu bom")
+        }
+
+    }
+
+}
---
+++ src/js/login.js
@@ -0,0 +1,26 @@
+function ValidarLogin(){
+    const email = registroMuseu.value
+    const senha = input_senha.value
+
+    if (email.indexOf("@") == -1){
+        alert("Seu email não possui @")
+    } else if (email.indexOf(".") == -1){
+        alert("Seu email precisa ter um ' . ' ")
+    } 
+    
+    if (senha.length <= 4){
+        alert("Sua senha precisa ter mais que 4 caracteres")
+    } else {
+        for(let numero = 0; numero <= 9; numero ++ ){
+            if(senha.indexOf(numero.toString())!= -1){
+                senhaNum = true
+            } 
+        }
+        if(senhaNum == false){
+            alert("Senha precisa de um caracter numérico")
+        } else {
+            alert("Deu bom")
+        }
+
+    }
+}
