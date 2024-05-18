a7444e78fb90f351604788655fc10aa83cc21d7c
nathannolacio <nathannolacio04@gmail.com>
2024-05-18 18:52:02 -0300

    Desenvolvido funções para mostrar e esconder a legenda através do botão 'ver legendas'

---
+++ public/js/dashboard-setor.js
@@ -0,0 +1,39 @@
+const headerContainer = document.querySelector('.header-container')
+let legendaAtiva = true;
+
+function mostrarLegendas() {
+
+    if (legendaAtiva == true) {
+        esconderLegendas()
+    } else {
+        headerContainer.innerHTML += `
+            <div class="metricas">
+                <div class="temperatura">
+                    <span class="titulo-metrica">Métricas de temperatura do setor</span>
+                    <div class="descricao">
+                        <p>Temperatura máxima: 00ºC</p>
+                        <p>Temperatura mínima: 00ºC</p>
+                    </div>
+                </div>
+                <div class="umidade">
+                    <span class="titulo-metrica">Métricas de umidade do setor</span>
+                    <div class="descricao">
+                        <p>Umidade máxima: 00ºC</p>
+                        <p>Umidade mínima: 00ºC</p>
+                    </div>
+                </div>
+            </div>
+        `
+        legendaAtiva = true;
+    }
+}
+
+function esconderLegendas() {
+    headerContainer.innerHTML = `
+        <div class="principal">
+            <span class="titulo-header">Setor 00 - Nome do Setor</span>
+            <button class="btn-verLegenda" onclick="mostrarLegendas()">Ver legendas</button>
+        </div>
+    `
+    legendaAtiva = false;
+}
