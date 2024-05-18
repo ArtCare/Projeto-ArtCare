9a07619fe3cea153936a746e8b8ef7a9f41f538e
VMDGAB <gmatosdantas777@gmail.com>
2024-05-18 13:21:16 -0300

    Validações de login e cadastro

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -1,61 +1,84 @@
 const conteudoCadastro = document.querySelector('#conteudoCadastro')
-const nome = input_nome.value
-const cnpj = input_cnpj.value
-const rm = input_rm.value
-const senha = input_senha.value
+const cadastroRepresentante = document.querySelector('#cadastroRepresentante')
+const message = document.querySelector('#message')
+const nome = input_nome
+const cnpj = input_cnpj
+const rm = input_rm
+const senha = input_senha
 
-function cadastro() {
-    let senhaNum = false
+const modal = document.querySelector('#alertModal')
+const sucesso = document.querySelector('#sucesso')
 
-    if(nome.length <= 2){
-        alert("Seu nome precisa ter mais que 2 caracteres")
-    } else if (cnpj.length != 14){
-        alert("Seu cnpj precisa ter 14 dígitos")
-    } else if (rm.length <= 8 || rm.length >= 10){
-        alert("Registro de museu inválido")
-    } else if (senha.length <= 4){
-        alert("Sua senha precisa ter mais que 4 caracteres")
+const nomeRep = document.querySelector('#nomeRepresentante')
+const emailRepresentante = document.querySelector('#email')
+
+const password = document.querySelector('#senhaRepresentante')
+const confSenha = document.querySelector('#confSenha')
+
+function nextStep() {
+     if (nome.value.length <= 2) {
+        modal.showModal()
+        message.textContent = "Seu nome precisa ter mais que 2 caracteres."
+    } else if (cnpj.value.length != 14) {
+        modal.showModal()
+        message.textContent = "Seu cnpj precisa ter 14 dígitos."
+    } else if (rm.value.length <= 8 || rm.value.length >= 10) {
+        modal.showModal()
+        message.textContent = "Registro de museu inválido."
+    } else if (senha.value.length <= 4) {
+        modal.showModal()
+        message.textContent = "Sua senha precisa ter mais que 4 caracteres."
     } else {
-        for(let numero = 0; numero <= 9; numero ++ ){
-            if(senha.indexOf(numero.toString())!= -1){
+        for (let numero = 0; numero <= 9; numero++) {
+            if (senha.value.indexOf(numero.toString()) != -1) {
                 senhaNum = true
-            } 
+            }
         }
-        if(senhaNum == false){
-            alert("Senha precisa de um caracter numérico")
+        if (senhaNum == false) {
+            modal.showModal()
+            message.textContent = "Senha precisa de um caracter numérico"
         } else {
-            window.location = "login.html"
+            conteudoCadastro.style.display = "none";
+            cadastroRepresentante.style.display = "flex";
         }
-
     }
 
 }
 
-
-function nextStep(){
-   conteudoCadastro.innerHTML = `<div id="cadastroRepresentante" class="cadastroRepresentante" data-aos="fade-left" data-aos-duration="1500">
-   <h1>Cadastre um <br>representante</h1>
-   <div  class="box-input ">
-       <div class="box-children-input">
-           <span>Nome</span>
-           <input class="input-Cadastro" type="text" id="input_nome">
-       </div>
-       <div class="box-children-input">
-           <span>Email</span>
-           <input class="input-Cadastro" type="text" id="input_rm">
-       </div>
-       <div class="box-children-input">
-           <span>Senha</span>
-           <input class="input-Cadastro" type="text" id="input_cnpj">
-       </div>
-       <div class="box-children-input">
-           <span>Confirmar senha</span>
-           <input class="input-Cadastro" type="password" id="input_senha">
-       </div>
-       <div class="btnDiv">
-           <button class="btnForm" onclick="cadastro()">CADASTRE-SE</button>
-       </div>
-   </div>
-</div>`
+function cadastro() {
+    let senhaNum = false
+    console.log(email)
+    console.log(nomeRep.value)
+    if (nomeRep.value.length <= 3) {
+        modal.showModal()
+        message.textContent = "Seu nome precisa ter mais que 3 caracteres."
+    } else if (emailRepresentante.value.indexOf('@') == -1 || emailRepresentante.value.indexOf('.') == -1) {
+        modal.showModal()
+        message.textContent = "Email inválido."
+    } else if (password.value.length <= 4) {
+        modal.showModal()
+        message.textContent = "Sua senha precisa ter mais que 4 caracteres."
+    } else {
+        for (let numero = 0; numero <= 9; numero++) {
+            if (senha.value.indexOf(numero.toString()) != -1) {
+                senhaNum = true
+            }
+        }
+        if (senhaNum == false) {
+            modal.showModal()
+            message.textContent = "Senha precisa de um caracter numérico"
+        } else {
+            sucesso.showModal()
+            setTimeout(() => {
+                location.replace('./login.html')
+            }, 3000)
+        }
+    }
 }
+
+function closeModal() {
+    modal.close()
+}
+
+
 
