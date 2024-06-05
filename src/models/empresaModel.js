c8efa9f22e395774f26843910d65761f438a92c6
RyanTorres05 <ryan.costa@sptech.school>
2024-06-05 14:55:53 -0300

    Alterando tempo de animação

---
+++ src/models/empresaModel.js
@@ -0,0 +1,16 @@
+var database = require("../database/config");
+
+function buscarPorId(id) {
+  var instrucaoSql = `SELECT * FROM empresa WHERE id = '${id}'`;
+
+  return database.executar(instrucaoSql);
+}
+
+function listar() {
+  var instrucaoSql = `SELECT * FROM empresa`;
+
+  return database.executar(instrucaoSql);
+}
+
+
+module.exports = { buscarPorId, listar };
