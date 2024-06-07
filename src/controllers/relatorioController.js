1b0d76ecaf8e9c1336808ee439158099781551b3
nathannolacio <nathannolacio04@gmail.com>
2024-06-07 14:22:51 -0300

    Função de pesquisar por nome do supersivor no arquivo relatorioController.js

--- src/controllers/relatorioController.js
+++ src/controllers/relatorioController.js
@@ -129,7 +129,25 @@ function deletar(req, res) {
         );
 }
 
+function pesquisarPorNome(req, res) {
+    const fkMuseu = req.params.fkMuseu
+    const nomePesquisado = req.body.nomeSupervisor
+
+    relatorioModel.pesquisarPorNome(fkMuseu, nomePesquisado).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
 module.exports = {
     buscarRelatorioVisualizacao,
-    inserirRelatorioVisualizacao
+    inserirRelatorioVisualizacao,
+    pesquisarPorNome
 }
