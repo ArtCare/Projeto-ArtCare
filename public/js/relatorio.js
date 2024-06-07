f2324c29bdbce86b3884ad856df52c36e99c4d38
nathannolacio <nathannolacio04@gmail.com>
2024-06-07 14:19:12 -0300

    Função de pesquisar setor por nome

--- public/js/relatorio.js
+++ public/js/relatorio.js
@@ -27,3 +27,48 @@ function logout() {
     sessionStorage.clear()
     window.location.href = "../login.html"
 }
+
+function pesquisarPorNome() {
+    fetch(`/relatorio/pesquisarPorNome/${fkMuseu}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nomeSupervisor: nomePesquisado.value
+        })
+    }).then(function (resposta) {
+
+        if (resposta.ok) {
+
+            resposta.json().then(res => {
+                dadosSupervisoresContainer.innerHTML = ''
+                for(posicao = 0; posicao < res.length; posicao++) {
+                    dadosSupervisoresContainer.innerHTML += `
+                    <div class="dadosSupervisores">
+                    <div class="div">
+                    <span class="nameSupervisor" id="nome">${res[posicao].nomeSupervisor}</span>
+                    </div>
+                    <div class="div">
+                    <span class="setor" id="setor">${res[posicao].nomeSetor}</span>
+                    </div>
+                    <div class="div">
+                    <span class="dataHora" id="dataHora">${res[posicao].dataHora}</span>
+                    </div>
+                    </div>
+                    `
+                }  
+            })
+        } else {
+            resposta.text().then(texto => {
+                console.error(texto);
+            });
+
+            dadosSupervisoresContainer.innerHTML = 'Nenhum registro foi encontrado!'
+        }
+    }).catch(function (erro) {
+        console.log(erro)
+    })
+
+    return false
+}
